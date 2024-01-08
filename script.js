1ba4d0693c9b70264c739ab005fb69ca76749cde
Min Dator <mejlbarai@gmail.com>
2024-01-08 21:09:57 +0100

    responsive to mibile and pads

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 bar = document.getElementById("bar");
 nav = document.getElementById("navbar");
 close = document.getElementById("close");
+bag = document.getElementById("lg-bag");
+bag_mobile = document.getElementById("lg-bag-mobile");
+
 
 if(bar){
     bar.addEventListener('click', () => {
         nav.classList.add('active');
-        close.classList.add('active');
+        bar.style.display = "none";
+        bag.style.display = "none";
+        bag_mobile.style.display = "none";
+        
     });
 }
 
 if(close){
     close.addEventListener('click', () => {
         nav.classList.remove('active');
+        bar.style.display = "flex";
+        bag.style.display = "flex";
+        bag_mobile.style.display = "flex";
     });
 }
